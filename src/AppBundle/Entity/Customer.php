5c58b2df4d18693f4aacada74c724aefbfb1981a
Michael Simmons <github@e7i.org>
2015-09-21 14:42:05 -0500

    Doctrine Entities: Customer and PizzaOrder

---
+++ src/AppBundle/Entity/Customer.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Customer
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Customer
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fname", type="string", length=255)
+     */
+    private $fname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lname", type="string", length=255)
+     */
+    private $lname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255)
+     */
+    private $phone;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fname
+     *
+     * @param string $fname
+     *
+     * @return Customer
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+
+        return $this;
+    }
+
+    /**
+     * Get fname
+     *
+     * @return string
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * Set lname
+     *
+     * @param string $lname
+     *
+     * @return Customer
+     */
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+
+        return $this;
+    }
+
+    /**
+     * Get lname
+     *
+     * @return string
+     */
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return Customer
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+}
+
---
+++ src/AppBundle/Entity/PizzaOrder.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PizzaOrder
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class PizzaOrder
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="customer_id", type="integer")
+     */
+    private $customerId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pizza_variety", type="string", length=255)
+     */
+    private $pizzaVariety;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="toppings", type="text")
+     */
+    private $toppings;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255)
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set customerId
+     *
+     * @param integer $customerId
+     *
+     * @return PizzaOrder
+     */
+    public function setCustomerId($customerId)
+    {
+        $this->customerId = $customerId;
+
+        return $this;
+    }
+
+    /**
+     * Get customerId
+     *
+     * @return integer
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * Set pizzaVariety
+     *
+     * @param string $pizzaVariety
+     *
+     * @return PizzaOrder
+     */
+    public function setPizzaVariety($pizzaVariety)
+    {
+        $this->pizzaVariety = $pizzaVariety;
+
+        return $this;
+    }
+
+    /**
+     * Get pizzaVariety
+     *
+     * @return string
+     */
+    public function getPizzaVariety()
+    {
+        return $this->pizzaVariety;
+    }
+
+    /**
+     * Set toppings
+     *
+     * @param string $toppings
+     *
+     * @return PizzaOrder
+     */
+    public function setToppings($toppings)
+    {
+        $this->toppings = $toppings;
+
+        return $this;
+    }
+
+    /**
+     * Get toppings
+     *
+     * @return string
+     */
+    public function getToppings()
+    {
+        return $this->toppings;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return PizzaOrder
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
