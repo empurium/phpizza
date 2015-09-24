38c6156cb3dda680b87bb1970837e2b33ea71026
Michael Simmons <github@e7i.org>
2015-09-24 01:32:39 -0500

    Form OrderType: playing with Symfony Forms, basic Order Form built

---
+++ src/AppBundle/Form/OrderType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('customer_id', 'integer', array(
+                'description' => 'Customer ID for this pizza Order.',
+            ))
+            ->add('pizza_variety', 'string', array(
+                'description' => 'Pizza Variety to base the additional toppings upon.',
+            ))
+            ->add('toppings', 'array', array(
+                'description' => 'Toppings that will go on top of the pizza.',
+            ))
+            ->add('status', 'string', array(
+                'description' => 'Status of the pizza Order.',
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'         => 'AppBundle\Entity\Order',
+            'intention'          => 'pizza_order',
+            'translation_domain' => 'AppBundle'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'pizza_order';
+    }
+}
