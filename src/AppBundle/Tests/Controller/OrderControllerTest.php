ae1db2c503fcee2cbd0ab16dd397f1f73fce08ba
Michael Simmons <github@e7i.org>
2015-09-24 14:13:39 -0500

    OrderControllerTest: Updated to use new POST API!

--- src/AppBundle/Tests/Controller/OrderControllerTest.php
+++ src/AppBundle/Tests/Controller/OrderControllerTest.php
@@ -6,11 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class OrderControllerTest extends WebTestCase
 {
-    public function testOrder()
+    public function testPostRest()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/order');
-    }
+        $crawler  = $client->request('POST', '/orders.json', array(
+            'fname' => 'Test First',
+            'lname' => 'Test Last',
+            'phone' => '555-555-1234',
+            'variety' => 'vegetarian',
+            'toppings' => array('green-olives', 'mushrooms'),
+        ));
+        $response = $client->getResponse();
+        print($response->getContent());
 
+        // ensure 200 status code from request
+        $this->assertEquals(200, $response->getStatusCode(), $response->getContent());
+
+        // ensure hardcoded value response
+        $order = json_decode($response->getContent(), true);
+        $this->assertEquals($order["status"], 'success');
+    }
 }
